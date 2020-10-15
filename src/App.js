416cff48f351e769aee4c9c5d7b10ba50b5b41df
occam98 <john.burk98@gmail.com>
2020-10-14 22:25:13 -0400

    Added button to add force

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React from "react";
 import "./styles.css";
 import Arrow from "./Arrow";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Free Body Diagram</h1>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 100">
-        <Arrow x="50" />
-        <Arrow x="100" />
-      </svg>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      children: [<Arrow x="50" />, <Arrow x="100" />]
+    };
+  }
+
+  appendChild() {
+    this.setState({
+      children: [...this.state.children, <Arrow x="75" />]
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Free Body Diagram</h1>
+        <button onClick={() => this.appendChild()}>Add force </button>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 100">
+          {this.state.children.map((child) => child)}
+        </svg>
+      </div>
+    );
+  }
 }
+
+export default App;
